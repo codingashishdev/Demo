2bb040fa6b501706aaea98715d7b99b35ccb3db4
Ashish Prajapati <108178021+ashCode98@users.noreply.github.com>
2025-01-30 12:09:22 +0530

    Changed something in the file

--- index.js
+++ index.js
@@ -1,47 +1,44 @@
 class TodoList {
-    constructor() {
-        this.todos = [];
-    }
+  constructor() {
+    this.todos = [];
+  }
 
-    addTodo(task) {
-        this.todos.push({
-            id: Date.now(),
-            task: task,
-            completed: false
-        });
-        console.log('Task added successfully!');
-    }
+  addTodo(task) {
+    this.todos.push({
+      id: Date.now(),
+      task: task,
+      completed: false,
+    });
+    console.log("Task added successfully!");
+  }
 
-    removeTodo(id) {
-        this.todos = this.todos.filter(todo => todo.id !== id);
-        console.log('Task removed successfully!');
-    }
+  toggleComplete(id) {
+    this.todos = this.todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+  }
 
-    toggleComplete(id) {
-        this.todos = this.todos.map(todo => {
-            if (todo.id === id) {
-                return { ...todo, completed: !todo.completed };
-            }
-            return todo;
-        });
+  displayTodos() {
+    if (this.todos.length === 0) {
+      console.log("No tasks in the list!");
+      return;
     }
-
-    displayTodos() {
-        if (this.todos.length === 0) {
-            console.log('No tasks in the list!');
-            return;
-        }
-        console.log('Current Tasks:');
-        this.todos.forEach(todo => {
-            console.log(`[${todo.completed ? 'X' : ' '}] ${todo.task} (ID: ${todo.id})`);
-        });
-    }
+    console.log("Current Tasks:");
+    this.todos.forEach((todo) => {
+      console.log(
+        `[${todo.completed ? "X" : " "}] ${todo.task} (ID: ${todo.id})`
+      );
+    });
+  }
 }
 
 // Example usage:
 const myTodoList = new TodoList();
-myTodoList.addTodo('Learn JavaScript');
-myTodoList.addTodo('Build a project');
+myTodoList.addTodo("Learn JavaScript");
+myTodoList.addTodo("Build a project");
 myTodoList.displayTodos();
 myTodoList.toggleComplete(myTodoList.todos[0].id);
 myTodoList.displayTodos();
